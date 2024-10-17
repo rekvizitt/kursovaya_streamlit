74570c034cdf0b6094356b2787b61175b2823ba0
rekvizitt <chekaviyvlad@gmail.com>
2024-10-17 12:27:46 +0300

    Change page names

--- pages/Страница_2.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(
-    page_title="Страница 2",
-    page_icon="❄️",
-)
-
-st.markdown("# Страница 2 ❄️")
-st.sidebar.markdown("# Страница 2 ❄️")
--- pages/Страница_3.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(
-    page_title="Страница 3",
-    page_icon="🎉",
-)
-
-st.markdown("# Page 3 🎉")
-st.sidebar.markdown("# Page 3 🎉")
